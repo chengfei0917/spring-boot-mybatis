6c2df610c5339441f657e1bc19db3ecd5358bbae
gg <861841648@qq.com>
2015-08-30 17:28:34 +0800

    sh

--- bin/run.sh
+++ bin/run.sh
@@ -5,7 +5,7 @@ then
  profile=$1
 fi
 echo $profile
-cd $( dirname $0 && pwd )
+cd $( dirname $0 )
 cd ../target/ 
 nohup java -jar -Dspring.profiles.active=$profile spring-boot-mybatis-1.0.0.jar >> ../logs/catalina.out 2>../logs/error.log &
 echo $!>../logs/pid.log
